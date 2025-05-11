8be3f920cf1474fc3896feabfbeb212f188dec15
Peter <peter.warholm@gmail.com>
2025-05-11 12:43:35 +0200

    Create mock data

---
+++ scripts/data.js
@@ -0,0 +1,48 @@
+const mockData = {
+    songs: [
+        {
+            id: 1,
+            title: "Bohemian Rhapsody",
+            artist: "Queen",
+            genre: "Rock"
+        },
+        {
+            id: 2,
+            title: "Stairway to Heaven",
+            artist: "Led Zeppelin",
+            genre: "Rock"
+        },
+        {
+            id: 3,
+            title: "Imagine",
+            artist: "John Lennon",
+            genre: "Pop"
+        },
+        {
+            id: 4,
+            title: "Like a Rolling Stone",
+            artist: "Bob Dylan",
+            genre: "Folk Rock"
+        },
+        {
+            id: 5,
+            title: "Billie Jean",
+            artist: "Michael Jackson",
+            genre: "Pop"
+        },
+        {
+            id: 6,
+            title: "Good Vibrations",
+            artist: "The Beach Boys",
+            genre: "Pop"
+        }
+    ],
+    
+    playlists: [
+        {
+            id: 1,
+            name: "In my ears",
+            songs: [1, 2, 6]
+        }
+    ]
+};
