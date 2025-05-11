7d31cb3346b37cc08443f96ddf06967aafb9aa54
Peter <peter.warholm@gmail.com>
2025-05-11 19:57:20 +0200

    Implement PlaylistsView for playlist management

---
+++ scripts/view/PlaylistsView.js
@@ -0,0 +1,22 @@
+import View from "./View";
+
+export default class PlaylistsView extends View {
+    constructor() {
+        super(document.getElementById('playlists'));
+        this._currentPlaylistElement = document.getElementById('current-playlist');
+        this._playlistNameInput = document.getElementById('playlist-name');
+        this._createPlaylistButton = document.getElementById('create-playlist');
+    }
+
+    _generateHtml() {
+        return `
+            ${this._data.playlists.map(playlist => `
+                <li class="playlist-item ${playlist.id === this._data.currentPlaylistId ? 'selected' : ''}" 
+                    data-id="${playlist.id}">
+                    <span>${playlist.name}</span>
+                    <button class="remove-btn" data-action="remove-playlist">Ta bort</button>
+                </li>
+            `).join('')}
+        `;
+    }
+}
