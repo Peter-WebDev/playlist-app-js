00d2d5d3313f48646fb5875e17a33d3e90093832
Peter <peter.warholm@gmail.com>
2025-05-11 19:28:11 +0200

    Implement generateHtml method

--- scripts/view/LibrarySongsView.js
+++ scripts/view/LibrarySongsView.js
@@ -1,7 +1,32 @@
-import View from './View'
+import View from './View';
 
 export default class LibrarySongsView extends View {
     constructor() {
         super(document.getElementById('library-songs'))        
+    }
+
+    _generateHtml() {
+        const { songs, playlistSongIds} = this._data;
+
+        return `
+            ${songs.map(song => {
+                const isInPlaylist = playlistSongIds.includes(song.id);
+
+                return `
+                    <li data-id='${song.id}'>
+                        <div class='song-details>
+                            <div class='song-title'>${song.title}</div>
+                            <div class='song-artist'>Artist: ${song.artist}</div>
+                            <div class='song-genre'>Genre: ${song.genre}</div>
+                        </div>
+                        <button class="${isInPlaylist ? 'disabled-btn' : 'add-btn'}" 
+                                data-action="add-song" 
+                                ${isInPlaylist ? 'disabled' : ''}>
+                            ${isInPlaylist ? 'Redan tillagd' : 'Lägg till i spellista'}
+                        </button>
+                    </li>
+                `;
+            }).join('')}
+        `;
     }
 }
