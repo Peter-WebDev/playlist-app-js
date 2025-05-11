89c3c4204cbe2bfc771462d1b18ec407fd1a559a
Peter <peter.warholm@gmail.com>
2025-05-11 16:21:35 +0200

    Add methods render + _clearHtml

--- scripts/views/View.js
+++ scripts/views/View.js
@@ -1,7 +1,16 @@
 class View {
     _parentElement = undefined;
+    _data;
 
     constructor(parentElement) {
         this._parentElement = parentElement
+    }
+
+    render(data) {
+
+    }
+
+    _clearHtml() {
+        
     }
 }
