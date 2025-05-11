d6f6e07f641029523ccc764ff59d86ef0a68977e
Peter <peter.warholm@gmail.com>
2025-05-11 16:34:30 +0200

    Revert "Add methods render + _clearHtml"
    This reverts commit 89c3c4204cbe2bfc771462d1b18ec407fd1a559a.

--- scripts/views/View.js
+++ scripts/views/View.js
@@ -1,16 +1,7 @@
 class View {
     _parentElement = undefined;
-    _data;
 
     constructor(parentElement) {
         this._parentElement = parentElement
-    }
-
-    render(data) {
-
-    }
-
-    _clearHtml() {
-        
     }
 }
