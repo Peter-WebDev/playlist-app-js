59a8557a60fee134e8b12ef855533d35acc16e35
Peter <peter.warholm@gmail.com>
2025-05-12 13:40:14 +0200

    Implement methods for create, select and delete playlists

--- scripts/controllers/musicController.js
+++ scripts/controllers/musicController.js
@@ -9,8 +9,28 @@ const musicController = {
 
         this.playlistView = new PlaylistView();
         this.playlistSongsView = new PlaylistSongsView();
-        this.librarySongsView = new LibrarySongsView(); 
-    }
+        this.librarySongsView = new LibrarySongsView();
+    },
+
+    onPlaylistChange() {
+        // Code
+    },
+
+    handleCreatePlaylist(name) {
+        musicModel.createPlaylist(name);
+        this.playlistView.clearPlaylistNameInput();
+        this.onPlaylistChange();
+    },
+
+    handleSelectPlaylist(playlistId) {
+        musicModel.setCurrentPlaylist(playlistId);
+        this.onPlaylistChange();
+    },
+
+    handleDeletePlaylist(playlistId) {
+        musicModel.deletePlaylist(playlistId);
+        this.onPlaylistChange();
+    },
 }
 
 export default musicController;
