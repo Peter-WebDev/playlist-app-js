bcb1fa7d3ee9f5b2433c6c3982ef4c887dcc9802
Peter <peter.warholm@gmail.com>
2025-05-13 22:52:34 +0200

    Add loadData and removeData

--- scripts/services/localStorage.js
+++ scripts/services/localStorage.js
@@ -13,4 +13,24 @@ class LocalStorage {
             return false;
         }
     }
+
+    loadData() {
+        try {
+            const serializeData = localStorage.getItem(this.storageKey);
+            return serializeData ? JSON.parse(serializeData) : null;
+        } catch (error) {
+            console.error('Error loading from localStorage', error);
+            return null;
+        }
+    }
+
+    removeData() {
+        try {
+            localStorage.removeItem(this.storageKey);
+            return true;
+        } catch (error) {
+            console.error('Error removing from localStorage', error);
+            return false;
+        }
+    }
 }
